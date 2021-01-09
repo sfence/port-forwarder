85c401c1118054af2ac324a7e0f251edcd898297
SFENCE <sfence.software@gmail.com>
2021-01-09 10:21:38 +0100

    Fix it to work with minetest server via udp.

--- redirect-udp_IPv4_IPv6.py
+++ redirect-udp_IPv4_IPv6.py
@@ -17,29 +17,33 @@ clients_dict = dict();
 
 class RedirectTargetSource(threading.Thread):
   def __init__(self, config):
-    threading.thread.__init__(self)
-    self.target_socket = config['redirect_socket']
-    self.source_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    threading.Thread.__init__(self)
+    print(config)
+    self.target_socket = config['target_socket']
+    self.source_socket = config['source_socket']
+    #self.source_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.source_addr = config['source_addr']
-    print("*** Listening IPv6 for {}:{}".format( self.source_addr[0], self.source_addr[1] ))
     self.lifetime = time.time()+5.0;
     self.nowtime = self.lifetime;
   
   def run(self):
+    print("*** Listening IPv6 listen for {}:{}".format(self.source_addr[0], self.source_addr[1] ))
     while self.lifetime>=self.nowtime:
       try:
-        data, addr = self.target_socket.recvfrom();
+        data, addr = self.target_socket.recvfrom(bufsize);
         if data:
+          #print("*** Responding: '{}' from addr {} to addr {}".format(data, addr, self.source_addr));
           self.source_socket.sendto(data, self.source_addr);
-        self.lifetime = time.time()+5.0;
+          self.lifetime = time.time()+5.0;
       except:
         # ignore exception
         pass;
-      self.nowtime = time.time();
+      finally:
+      	self.nowtime = time.time();
     
-    del socket_target_source[self.source_addr]
+    del clients_dict[self.source_addr]
     self.target_socket.close();
-    self.source_socket.close();
+    print("*** Close IPv6 for {}:{}".format( self.source_addr[0], self.source_addr[1] ))
 
 def listen(host, port):
   listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -47,16 +51,20 @@ def listen(host, port):
   print("*** Listening IPv4 on {}:{}".format( host, port ))
   while True:
     data, addr = listen_socket.recvfrom(bufsize)
-    if addr not in socket_target_source:
+    if addr not in clients_dict:
       target_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM);
-      clients_dict[addr] = dict(target_socket=target_socket, source_addr=addr);
+      target_socket.setblocking(0);
+      clients_dict[addr] = dict(target_socket=target_socket, source_socket=listen_socket, source_addr=addr);
       thread = RedirectTargetSource(clients_dict[addr])
       thread.start();
     else:
-      target_socket = clients_dict[addr][target_socket];
+      target_socket = clients_dict[addr]['target_socket'];
     if data:
-      print("*** Redirecting: '{}' from port {}".format(data, addr[1]));
-      target_socket.sendto(data, (target_host, target_port));
+      #print("*** Redirecting: '{}' from addr {} to addr {}".format(data, addr, (target_host, target_port)));
+      try:
+        target_socket.sendto(data, (target_host, target_port));
+      except:
+        pass;
 
 listen(listen_host, listen_port)
 
